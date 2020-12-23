86c7664fca60e8e63855944c8d4eeb8f29bcfba6
Nirajp428 <70656760+Nirajp428@users.noreply.github.com>
2020-12-22 20:32:55 -0500

    Add files via upload

--- Room.java
+++ Room.java
@@ -1,9 +1,16 @@
 package server;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+
+
 
 public class Room implements AutoCloseable {
     private static SocketServer server;// used to refer to accessible server functions
@@ -14,9 +21,18 @@ public class Room implements AutoCloseable {
     private final static String COMMAND_TRIGGER = "/";
     private final static String CREATE_ROOM = "createroom";
     private final static String JOIN_ROOM = "joinroom";
+   
+   
+    public Room(String name, boolean delayStart) {
+	
+	this.name = name;
+	
+    }
 
     public Room(String name) {
 	this.name = name;
+	// set this for BaseGamePanel to NOT draw since it's server-side
+	
     }
 
     public static void setServer(SocketServer server) {
@@ -27,29 +43,37 @@ public class Room implements AutoCloseable {
 	return name;
     }
 
-    private List<ServerThread> clients = new ArrayList<ServerThread>();
+    
 
     protected synchronized void addClient(ServerThread client) {
 	client.setCurrentRoom(this);
-	if (clients.indexOf(client) > -1) {
-	    log.log(Level.INFO, "Attempting to add a client that already exists");
-	}
-	else {
-	    clients.add(client);
-	    if (client.getClientName() != null) {
-		sendMessage(client, "joined the room " + getName());
-	    }
-	}
+	boolean exists = false;}
+	// since we updated to a different List type, we'll need to loop through to find
+	// the client to check against
+	
+	
+
+
+   
+
+    /***
+     * Syncs the existing players in the room with our newly connected player
+     * 
+     * @param client
+     */
+   
+
+    /**
+     * Syncs the existing clients in the room with our newly connected client
+     * 
+     * @param client
+     */
+    private synchronized void updateClientList(ServerThread client) {
+	
     }
 
     protected synchronized void removeClient(ServerThread client) {
-	clients.remove(client);
-	if (clients.size() > 0) {
-	    sendMessage(client, "left the room");
-	}
-	else {
-	    cleanupEmptyRoom();
-	}
+	
     }
 
     private void cleanupEmptyRoom() {
@@ -117,16 +141,8 @@ public class Room implements AutoCloseable {
 	return wasCommand;
     }
 
-    protected void sendConnectionStatus(String clientName, boolean isConnect) {
-	Iterator<ServerThread> iter = clients.iterator();
-	while (iter.hasNext()) {
-	    ServerThread client = iter.next();
-	    boolean messageSent = client.sendConnectionStatus(clientName, isConnect);
-	    if (!messageSent) {
-		iter.remove();
-		log.log(Level.INFO, "Removed client " + client.getId());
-	    }
-	}
+    protected void sendConnectionStatus(ServerThread client, boolean isConnect, String message) {
+	
     }
 
     /***
@@ -138,20 +154,45 @@ public class Room implements AutoCloseable {
      * @param message The message to broadcast inside the room
      */
     protected void sendMessage(ServerThread sender, String message) {
-	log.log(Level.INFO, getName() + ": Sending message to " + clients.size() + " clients");
+	
 	if (processCommands(message, sender)) {
 	    // it was a command, don't broadcast
 	    return;
 	}
-	Iterator<ServerThread> iter = clients.iterator();
-	while (iter.hasNext()) {
-	    ServerThread client = iter.next();
-	    boolean messageSent = client.send(sender.getClientName(), message);
-	    if (!messageSent) {
-		iter.remove();
-		log.log(Level.INFO, "Removed client " + client.getId());
-	    }
-	}
+	
+    }
+
+    /**
+     * Broadcasts this client/player direction to all connected clients/players
+     * 
+     * @param sender
+     * @param dir
+     */
+    protected void sendDirectionSync(ServerThread sender, Point dir) {
+	boolean changed = false;
+	// first we'll find the clientPlayer that sent their direction
+	// and update the server-side instance of their direction
+	
+	
+	// if the direction is "changed" (it should be, but check anyway)
+	// then we'll broadcast the change in direction to all clients
+	// so their local movement reflects correctly
+	
+	
+    }
+
+    /**
+     * Broadcasts this client/player position to all connected clients/players
+     * 
+     * @param sender
+     * @param pos
+     */
+    protected void sendPositionSync(ServerThread sender, Point pos) {
+	
+    }
+
+    public List<String> getRooms() {
+	return server.getRooms();
     }
 
     /***
@@ -160,21 +201,52 @@ public class Room implements AutoCloseable {
      */
     @Override
     public void close() throws Exception {
-	int clientCount = clients.size();
-	if (clientCount > 0) {
-	    log.log(Level.INFO, "Migrating " + clients.size() + " to Lobby");
-	    Iterator<ServerThread> iter = clients.iterator();
-	    Room lobby = server.getLobby();
-	    while (iter.hasNext()) {
-		ServerThread client = iter.next();
-		lobby.addClient(client);
-		iter.remove();
-	    }
-	    log.log(Level.INFO, "Done Migrating " + clients.size() + " to Lobby");
-	}
-	server.cleanupRoom(this);
-	name = null;
+	
+	}{
+	
+	name = null;}
 	// should be eligible for garbage collection now
+    
+
+   
+    public void awake() {
+	// TODO Auto-generated method stub
+
     }
+
+   
+    public void start() {
+	// TODO Auto-generated method stub
+	log.log(Level.INFO, getName() + " start called");
+    }
+
+    long frame = 0;
+
+
+
+    
+
+    
+    public void update() {
+	// We'll make the server authoritative
+	// so we'll calc movement/collisions and send the action to the clients so they
+	// can visually update. Client's won't be determining this themselves
+	
+
+    }
+
+    // don't call this more than once per frame
+    private void nextFrame() {
+	// we'll do basic frame tracking so we can trigger events
+	// less frequently than each frame
+	// update frame counter and prevent overflow
+	if (Long.MAX_VALUE - 5 <= frame) {
+	    frame = Long.MIN_VALUE;
+	}
+	frame++;
+    }
+
+   
+    
 
 }
